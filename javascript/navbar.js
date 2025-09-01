e3ecd0ab538ad2c8b10a40ec76d28cdcea06782c
mmrathaur <manmohanrathaur2001@gmail.com>
2025-09-01 15:29:15 +0530

    Update navbar.js

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,7 +1,6 @@
-fetch("../html/navbar.html")
+fetch("html/navbar.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("navbar").innerHTML = data;
-  });
-
-
+  })
+  .catch(error => console.error("Error loading navbar:", error));
